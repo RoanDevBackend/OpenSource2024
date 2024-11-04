edd5660e20ce03126d742fbf78f96228cb81f2f2
hung-manh <hmh290204@gmail.com>
2024-11-04 16:23:58 +0700

    init commit

---
+++ opensource2024.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: Web AI Builder by 10Web
+ * Plugin URI: 
+ * Description: Web AI Builder creates perfect, unique, and SEO-optimized content 10x faster. Use AI Assistant directly inside your WordPress environment.
+ * Version: 1.0.0
+ * Author: 
+ * Author URI:
+ * Text Domain:
+ * License: GPL2
+*/
+
