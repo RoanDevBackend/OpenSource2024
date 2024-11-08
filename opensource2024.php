5cfef8c29733629baf04009a3685f403596f2bc8
RoanVanQuyen <roan.dev.backend@gmail.com>
2024-11-09 00:23:33 +0700

    feat: Add plugin to WordPress admin menu
    - Implement OpenSource2024_Plugin class with singleton pattern
    - Add 'Open Source' menu item to WordPress admin dashboard
    - Include custom CSS file for admin styling
    - Set up action hooks for admin_menu and admin_enqueue_scripts

--- opensource2024.php
+++ opensource2024.php
@@ -1,12 +1,39 @@
 <?php
 /**
- * Plugin Name: Web AI Builder by 10Web
- * Plugin URI: 
+ * Plugin Name: Open Source 2024
+ * Plugin URI:
  * Description: Web AI Builder creates perfect, unique, and SEO-optimized content 10x faster. Use AI Assistant directly inside your WordPress environment.
  * Version: 1.0.0
- * Author: 
+ * Author:
  * Author URI:
  * Text Domain:
  * License: GPL2
 */
+
+if(!class_exists("OpenSource2024_Plugin")) {
+    class OpenSource2024_Plugin {
+        public function __construct() {
+            add_action('admin_menu', array($this, 'admin_menu'));
+            add_action("admin_enqueue_scripts", array($this, 'admin_enqueue_scripts'));
+        }
+
+        function admin_menu():void {
+            add_menu_page(
+                "AI Content Generation",
+                "Open Source",
+                "manage_options",
+                "home",
+                array($this, "render"),
+                plugin_dir_url(__FILE__)."assets/images/logo.png",
+                10000
+            );
+        }
+        function admin_enqueue_scripts():void {
+            wp_enqueue_style("opensource2024", plugin_dir_url(__FILE__)."assets/css/admin.css");
+        }
+    }
+    new OpenSource2024_Plugin();
+}
+
+
 
